fb168dc2f4b6f6b4ab0cde7e62fd6bb07c4db4ff
Mumintroll <fitton@mail.ru>
2022-12-06 19:21:55 +0400

    05_ERC20

---
+++ Solidity/02_TestApps/05_ERC20/ERC20.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./IERC20.sol";
+
+contract ERC20 is IERC20 {
+    uint public totalSupply;
+    mapping(address=>uint) public balanceOf;
+    mapping(address=>mapping(address=>uint)) public allowance;
+    string public constant name = "Solidity by example";
+    string public constant symbol = "SOLBYEX";
+    uint public constant decimals = 18;
+
+    function transfer(address recipient, uint amount) external returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Not enough tokens");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient]  += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint amount) external returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Not enough tokens");
+        allowance[msg.sender][spender] += amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient,uint amount) external returns (bool) {
+        require(allowance[sender][msg.sender] >= amount, "Not enough approved");
+        balanceOf[sender] -= amount;
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(uint amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint amount) external {
+        require(balanceOf[msg.sender] >= amount, "Too much to burn");
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
+
+}
+
